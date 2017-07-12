d4d209f877430804f663bea68dac2f4de691f3c8
Zhong Dai <zhongdai.au@gmail.com>
2017-07-12 21:03:28 +1000

    change design

--- wbteq/wbteq.py
+++ wbteq/wbteq.py
@@ -8,56 +8,29 @@ from datetime import datetime
 import os.path
 import re
 from . import __version__
+from io import TextIOWrapper
 
+class BTEQTemplate:
+    def __init__(self,fd=None):
+        if not isinstance(fd, TextIOWrapper) or fd is None:
+            raise ValueError('Please give a correct text file handler')
+        self._text = fd.read()
+        self._param_list = re.findall(r"\{[a-zA-Z_]+?\}", self._text)
 
-class BTEQ:
-    def __init__(self, filename=None, init_flag=False, exec_flag=False, prefix='wbteq_'):
-        if filename is None:
-            raise ValueError('Please give a file name as input')
-        self.filename = filename
-        self.init_flag = init_flag
-        self.exec_flag = exec_flag
-        self.prefix = prefix
-
-        with open(filename) as f:
-            full_text = f.read()
-
-        self.param_list = re.findall(r"\{.+?\}", full_text)
-
-
-    def _parse(self):
-        """
-        Get all ENVs
-        """
-        param = {}
-        for k, v in os.environ.items():
-            if k.lower().startswith(self.prefix):
-                param[k] = v
-        return param
-
-    def __repr__(self):
-        if self.init_flag is True:
-            return 'Inital BTEQ <{}>'.format(self.filename)
+    @property
+    def param_list(self):
+        if len(self._param_list) > 0:
+            return self._param_list
         else:
-            return 'Executable BTEQ <{}>'.format(self.filename)
+            return None
 
-    def __str__(self):
-        return self.__repr__()
-
-    def run(self):
-        if self.init_flag is True:
-            print('Setup the ENV variables {}'.format(self.filename))
-        else:
-            print('Run the BTEQ scrip {}'.format(self.filename))
 
 
 def get_parser():
     parser = argparse.ArgumentParser(description='run Teradata BTEQ on Windows')
-    parser.add_argument('filename', type=str,
-                        help='The parameterised BTEQ script file name')
+    parser.add_argument('filename', type=str, nargs='+',
+                        help='The parameterised BTEQ script file names')
     parser.add_argument('-v', '--version', help='displays the current version of wbteq',
-                        action='store_true')
-    parser.add_argument('-i','--init', help='this initial step to assign env variables',
                         action='store_true')
     parser.add_argument('-e','--exec', help='execute the BTEQ command',
                         action='store_true')
@@ -74,11 +47,7 @@ def command_line_runner():
         print(__version__)
         return
 
-    b = BTEQ(args['filename'],
-             args['init'],
-             args['exec'],
-             args['prefix'])
-    b.run()
+    print(args)
 
 
 if __name__ == '__main__':
